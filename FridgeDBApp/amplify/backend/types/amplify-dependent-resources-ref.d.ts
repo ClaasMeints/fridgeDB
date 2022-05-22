dd82bb62af25980709c7b9d3540b470f42ba3213
ClaasMeints <c.meints@stud.phwt.de>
2022-05-22 12:19:18 +0200

    added amplify

---
+++ FridgeDBApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "FridgeDBApp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "FridgeDBApp": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- FridgeDBApp/index.js
+++ FridgeDBApp/index.js
@@ -2,6 +2,11 @@
  * @format
  */
 
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
