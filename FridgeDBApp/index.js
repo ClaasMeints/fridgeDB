b8df457045506a71e04ccba7e119815f315ef7ad
ClaasMeints <c.meints@stud.phwt.de>
2022-05-28 22:30:18 +0200

    amplify test

--- FridgeDBApp/index.js
+++ FridgeDBApp/index.js
@@ -1,6 +1,10 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
 
-import App from './App';
+import App from "./App";
+
+import { Amplify } from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
